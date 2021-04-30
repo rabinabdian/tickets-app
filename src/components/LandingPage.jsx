90ce8928a8d9b499f4ff643e486d0fa0c6dc9399
MosDaro <shkilmoisei@gmail.com>
2021-04-30 22:46:15 +0300

    remove unused libs

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 import TicketsList from "../features/tickets/TicketsList";
-import { selectUser } from "../features/user/userSlice";
 
 export default function LandingPage({ user }) {
   return (
